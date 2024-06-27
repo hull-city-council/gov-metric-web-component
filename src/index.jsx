f613164e813ab62eabb18d218984e9487711eeff
hccdanny <117913283+HCCDanny@users.noreply.github.com>
2024-06-27 11:06:45 +0000

    Prepend to body

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,5 @@ import r2wc from "@r2wc/react-to-web-component";
 import LoadGovMetric from "./App";
 
 const govMetricRating = r2wc(LoadGovMetric, { props: { account: "string" } });
-
 customElements.define("gov-metric-rating", govMetricRating);
-const govMetricRatingElement = document.createElement("gov-metric-rating");
-document.getElementById("main").appendChild(govMetricRatingElement);
+document.body.prepend(document.querySelector("gov-metric-rating"));
